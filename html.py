29124f08faf51ccce9842fd9b0a6bf33039f1a9a
kunal galav <kunalgalav1995@gmail.com>
2023-07-14 17:54:09 +0530

    connect to bluethooth

---
+++ html.py
@@ -0,0 +1,30 @@
+Sure, here are the steps on how to connect to Bluetooth in an Android TV:
+
+1. Turn on your Android TV and make sure it is connected to the internet.
+2. Press the Home button on your remote control.
+3. Select the **Settings** icon.
+4. Scroll down and select **Remote & accessories**.
+5. Select **Add accessory**.
+6. Your TV will start scanning for Bluetooth devices.
+7. Put your Bluetooth device in pairing mode.
+8. Once your device is found, select it from the list.
+9. Enter the passcode if prompted.
+10. Your device will be connected to your Android TV.
+
+Here are some additional tips for connecting to Bluetooth in an Android TV:
+
+* Make sure that your Bluetooth device is turned on and discoverable.
+* If you're having trouble connecting your device, try restarting both your TV and your device.
+* If your device is not listed in the list of available devices, try putting it in pairing mode again.
+
+Once your device is connected to your Android TV, you can use it to listen to music, watch videos, or play games. You can also use it as a remote control for your TV.
+
+Here are some examples of Bluetooth devices that you can connect to your Android TV:
+
+* Headphones
+* Speakers
+* Game controllers
+* Keyboards
+* Mice
+
+I hope this helps! Let me know if you have any other questions.
