17aae04c17710eab4909468270960cdab5ae9406
kunal galav <kunalgalav1995@gmail.com>
2023-07-14 13:49:35 +0530

    testing programming

---
+++ programming.py
@@ -0,0 +1,8 @@
+age =19
+if age >= 18:
+    print ("you are in adult")
+    print ("you can vote")
+    print ("thank you")
+elif age<18 and age >3:
+    print ("you are in school")
+else:print("you are a child")
